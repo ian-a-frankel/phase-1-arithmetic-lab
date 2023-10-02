e6d09caebbd5f9dd3ed5f417304e43d54a9ddfe9
Ian Frankel <ian.frankel@gmail.com>
2023-10-02 12:08:06 -0400

    basic math ops

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(100 * Math.random())
+
+const num3 = 9
+const num4 = 5
+
+const mod = num3 % num4
+
+const num5 = 10
+const num6 = 20
+
+const max = Math.max(num5, num6)
